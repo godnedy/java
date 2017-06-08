841b92fe36461dae60402a446745ec4844acab55
Edyta <edyta.godniak@protonmail.ch>
2017-06-08 08:51:45 +0200

    Add files via upload

---
+++ Concurrency/Readers Writers Favours Writers/Writer.java
@@ -0,0 +1,34 @@
+package com.company;
+
+
+/**
+ * Created by Student3 on 2017-04-06.
+ */
+public class Writer implements Runnable{
+
+
+
+    private ReadingRoom rr;
+
+    public Writer(ReadingRoom rr){
+        this.rr = rr;
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            this.rr.prepareToWrite();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("I'am the thread nr " + Thread.currentThread().getName() + " and I am WRITING inside the ReadingRoom");
+        try {
+            Thread.sleep(rr.random.nextInt(20));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.rr.endWriting();
+
+    }
+}
